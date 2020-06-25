80b55e647cf4bbf9b934e84120847acf4f99f448
burwinliu <burwincollege@gmail.com>
2020-06-25 13:55:56 -0700

    Fixed routing

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import 'styles/App.css';
 import NavBar from 'components/componentNavbar'
 import Footer from "components/componentFooter";
-import {Router, Route} from "react-router";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import ViewHome from "./view/viewHome";
 import ViewUser from "./view/viewUser";
 import ViewFamily from "./view/viewFamily";
@@ -17,13 +21,15 @@ function App()  {
             <NavBar/>
             <Router>
                 <div className='body'>
-                    <Route path={"/"} exact component={ViewHome}/>
-                    <Route path={"/user"} exact component={ViewUser}/>
-                    <Route path={"/family"} exact component={ViewFamily}/>
-                    <Route path={"/search-user"} exact component={ViewSearchUser}/>
-                    <Route path={"/search-family"} exact component={ViewSearchFamily}/>
-                    <Route path={"/input-user"} exact component={ViewInputUser}/>
-                    <Route path={"/input-family"} exact component={ViewInputFamily}/>
+                    <Switch>
+                        <Route path={"/"} exact component={ViewHome}/>
+                        <Route path={"/user"} exact component={ViewUser}/>
+                        <Route path={"/family"} exact component={ViewFamily}/>
+                        <Route path={"/search-user"} exact component={ViewSearchUser}/>
+                        <Route path={"/search-family"} exact component={ViewSearchFamily}/>
+                        <Route path={"/input-user"} exact component={ViewInputUser}/>
+                        <Route path={"/input-family"} exact component={ViewInputFamily}/>
+                    </Switch>
                 </div>
             </Router>
             <Footer/>
