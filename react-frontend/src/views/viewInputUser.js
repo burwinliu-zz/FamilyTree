08805b2055b750b3936bc0dc87a6e10114cb2924
yarwinliu <yarwinliu@gmail.com>
2020-06-26 22:40:11 -0700

    storing changes

--- react-frontend/src/views/viewInputUser.js
+++ react-frontend/src/views/viewInputUser.js
@@ -1,11 +1,15 @@
 import React from "react";
 import TextField from '@material-ui/core/TextField';
 import 'styles/view.css'
+import Button from '@material-ui/core/Button';
 
 function ViewInputUser()  {
     return (
         <div className="view">
-            <TextField id="text" type="text" />
+            <TextField id="text" label="input user here" />
+            <Button variant="contained">
+            enter
+            </Button>
         </div>
     );
 }
